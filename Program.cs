1f8b8e03231d232780d97d72e3243926f36ec2ad
Fernanda Oliveira <141464172+junggtail@users.noreply.github.com>
2023-08-22 22:44:20 -0300

    Pastorzinho
    Atividade

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("dó, ");
+Console.Beep(1320, 200);
+Console.Write("ré, ");
+Console.Beep(1485, 200);
+Console.Write("mi, ");
+Console.Beep(1650, 200);
+Console.Write("fá, ");
+Console.Beep(1759, 200);
+Thread.Sleep(500);
+Console.White("fá, ");
+Console.Beep(1759, 200);
+Console.Write("fá,");
+Console.Beep(1759, 200);
+Thread.Sleep(1000);
+
+Console.Writeline();
+
+Console.Write("dó, ");
+Console.Beep(1320, 200);
+Console.Write("ré ");
+Console.Beep(1485, 200);
+Console.Write("dó,");
+Console.Beep(1320, 200);
+Console.Write("ré, ");
+Console.Beep(1485, 200);
+Thread.Sleep(1000);
+
+Console.Writeline();
+
+Console.Write("dó, ");
+Console.Beep(1320, 200);
+Console.Write("sol, ");
+Console.Beep(1980, 200);
+Console.Write("fá, ");
+Console.Beep(1759, 200);
+Console.White("mi, ");
+Console.Beep(1650, 200);
+Thread.Sleep(500);
+Console.Write("mi, ");
+Console.Beep(1650, 200);
+Console.Write("mi, ");
+Console.Beep(1650, 200);
+Thread.Sleep(1000);
+
+Console.Writeline();
+
+Console.Write("dó, ");
+Console.Beep(1320, 200);
+Console.Write("ré, ");
+Console.Beep(1485,200);
+Console.Write("mi, ");
+Console.Beep(1650,200);
+Console.Write("fá, ");
+Console.Beep(1759, 200);
+Thread.Sleep(500);
+Console.White("fá, ");
+Console.Beep(1759, 200);
+Console.Write("fá, ");
+Console.Beep(1759, 200);
+Thread.Sleep(1000);
+
+
+
